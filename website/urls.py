764a01c9ed1f32a7736810960d28aef75eb5aeac
alexHooper <alexmohack@gmail.com>
2018-09-27 18:24:05 +0530

    add url for signup view

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from .views import signup_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', signup_view, name='sign-up'),
 ]
