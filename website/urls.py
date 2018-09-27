458e40604a746de8cf64c5c0f764fc57d3a2985a
alexHooper <alexmohack@gmail.com>
2018-09-27 20:02:43 +0530

    add urls for home and dashboard

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import signup_view
+from .views import signup_view, dashboard_view, home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', signup_view, name='sign-up'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('', home_view, name='home'),
 ]
