e58162ff688ba6dab67e671d54dddff6f9190dd6
alexHooper <alexmohack@gmail.com>
2018-09-27 20:07:01 +0530

    add login view

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from .views import signup_view, dashboard_view, home_view
 
@@ -8,4 +9,8 @@ urlpatterns = [
     path('signup/', signup_view, name='sign-up'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('', home_view, name='home'),
+]
+
+urlpatterns += [
+    path('login/', LoginView.as_view(), name='login'),
 ]
