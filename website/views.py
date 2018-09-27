ed72f243739da29c98eb2509ab182d850edc301c
alexHooper <alexmohack@gmail.com>
2018-09-27 19:15:37 +0530

    correct error in signup view

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 
-def signup(request):
+def signup_view(request):
 	if request.method == "POST":
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
@@ -12,7 +12,7 @@ def signup(request):
 			user = authenticate(username=username, password=password)
 			login(request, user)
 			return redirect('home')
-		else:
-			form = UserCreationForm()
+	else:
+		form = UserCreationForm()
 
-		return render(request, 'signup.html', {'form': form})
+	return render(request, 'signup.html', {'form': form})
