e87ce0bea78942917fef74c200ed9571478ed5b2
alexHooper <alexmohack@gmail.com>
2018-09-27 18:46:29 +0530

    add signup view

--- website/views.py
+++ website/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			return redirect('home')
+		else:
+			form = UserCreationForm()
+
+		return render(request, 'signup.html', {'form': form})
