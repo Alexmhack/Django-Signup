ef6ace7a6c62150106f2cd471cad2679997e8479
alexHooper <alexmohack@gmail.com>
2018-09-27 18:40:39 +0530

    use env variables

--- website/settings.py
+++ website/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '93%@nka8)+fv-*ai-st1d*h)w2j2-y^)(jfiv9bogcy0u241u7'
+SECRET_KEY = config("PROJECT_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
