0679bb50fedc8b5b66c95840560c3bc3e0f68257
alexHooper <alexmohack@gmail.com>
2018-09-28 15:16:06 +0530

    add view for profile signup

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from mysite.core.forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
