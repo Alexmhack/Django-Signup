f42211d87e5d4bd9a66d43aebd6bd92891a450d6
alexHooper <alexmohack@gmail.com>
2018-09-28 18:33:20 +0530

    fetch and store location for user

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from .utils import get_location_from_ip
+
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	bio = models.CharField(max_length=50, blank=True)
@@ -16,4 +18,11 @@ class Profile(models.Model):
 def update_user_profile(sender, instance, created, **kwargs):
 	if created:
 		Profile.objects.create(user=instance)
+		user = Profile.objects.get(user=instance)
+		ip_address = user.location
+		location = get_location_from_ip(ip_address)
+		print(location)
+		print(ip_address)
+		user.location = location
+		user.save()
 	instance.profile.save()
