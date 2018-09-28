198d954fcc8fe2edee9bf37c6b1061cc61c478d9
alexHooper <alexmohack@gmail.com>
2018-09-28 08:39:57 +0530

    include urls for users app

--- website/urls.py
+++ website/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-
-from .views import signup_view, dashboard_view, home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', signup_view, name='sign-up'),
-    path('dashboard/', dashboard_view, name='dashboard'),
-    path('', home_view, name='home'),
+    path('', include('users.urls', namespace='users')),
 ]
 
 urlpatterns += [
