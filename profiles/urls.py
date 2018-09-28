5e0657384876a4be8dfd3f77808af16fe442b6c2
alexHooper <alexmohack@gmail.com>
2018-09-28 17:39:48 +0530

    add url for profile edit

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import signup
+from .views import signup, ProfileUpdateView
 
 urlpatterns = [
 	path('', signup, name='profile-signup'),
+	path('<int:pk>/edit/', ProfileUpdateView.as_view(), name='profile-edit'),
 ]
