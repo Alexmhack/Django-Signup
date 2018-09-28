671af221976e4e1231e05c1677f2df8c59efd341
alexHooper <alexmohack@gmail.com>
2018-09-28 18:57:43 +0530

    return city and country

--- profiles/utils.py
+++ profiles/utils.py
@@ -3,6 +3,8 @@ import requests
 def get_location_from_ip(ip_address):
 	try:
 		response = requests.get(f"http://ip-api.com/json/{ip_address}")
-		return response.json()
+		city = response.get("city")
+		country_code = response.get("countryCode")
+		return f"{city} - {country_code}"
 	except Exception as e:
-		print(e)
+		return "Failed fetching location"
