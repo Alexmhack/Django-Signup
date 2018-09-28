4bdc3c3265df45b1f0868c5ca96ed1224433ab3b
alexHooper <alexmohack@gmail.com>
2018-09-28 15:04:13 +0530

    add fields to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -2,3 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	bio = models.CharField(max_length=50, blank=True)
+	location = models.CharField(max_length=30, blank=True)
+
+	timestamp = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
