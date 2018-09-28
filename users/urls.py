f1e54cad3b67428a3f8c4f8812002efc22f608de
alexHooper <alexmohack@gmail.com>
2018-09-28 08:41:18 +0530

    add urls in users app

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+
+app_name = "users"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
     path('signup/', signup_view, name='sign-up'),
     path('dashboard/', dashboard_view, name='dashboard'),
-    path('', home_view, name='home'),
 ]
