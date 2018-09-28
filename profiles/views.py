11f56b79e267c392c102fe5d5656b3970b1c3b38
alexHooper <alexmohack@gmail.com>
2018-09-28 15:19:23 +0530

    minor changes

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
+
 from .forms import SignUpForm
 
 def signup(request):
