bb79792063cd2a4adfbb4fe53680fbe286aed717
alexHooper <alexmohack@gmail.com>
2018-09-28 17:40:26 +0530

    add django-widget-tweaks to settings

--- website/settings.py
+++ website/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
