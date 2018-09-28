72a21648db1cd5dabda06b9c348ef792e19be83d
alexHooper <alexmohack@gmail.com>
2018-09-28 16:21:11 +0530

    add template path in logout view

--- website/urls.py
+++ website/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
 
 urlpatterns += [
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
