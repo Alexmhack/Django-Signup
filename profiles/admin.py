cdb38fe3bcb1ad7e5e7d382c00f9c13432dcbef0
alexHooper <alexmohack@gmail.com>
2018-09-28 15:04:24 +0530

    register profile to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+@admin.register(Profile)
+class ProfileModelAdmin(admin.ModelAdmin):
+	list_display = ('user', 'location', 'timestamp')
+	list_search_fields = ('user', 'location')
