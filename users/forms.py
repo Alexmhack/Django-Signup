7ee64f6082d5b5d63b39da3061aa21dfa1907654
alexHooper <alexmohack@gmail.com>
2018-09-28 10:07:10 +0530

    remove first and last name from form

--- users/forms.py
+++ users/forms.py
@@ -3,31 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-	first_name = forms.CharField(
-		label='',
-		max_length=10,
-		min_length=4,
-		required=True,
-		widget=forms.TextInput(
-			attrs={
-				"placeholder": "First Name",
-				"class": "form-control"
-			}
-		)
-	)
-
-	last_name = forms.CharField(
-		label='',
-		max_length=30,
-		required=True,
-		widget=forms.TextInput(
-			attrs={
-				"placeholder": "Last Name",
-				"class": "form-control"
-			}
-		)
-	)
-
 	email = forms.EmailField(
 		label='',
 		max_length=254,
@@ -51,6 +26,7 @@ class SignUpForm(UserCreationForm):
 			}
 		)
 	)
+
 	password1 = forms.CharField(
 		label='',
 		max_length=30,
@@ -63,6 +39,7 @@ class SignUpForm(UserCreationForm):
 			}
 		)
 	)
+
 	password2 = forms.CharField(
 		label='',
 		max_length=30,
@@ -78,4 +55,4 @@ class SignUpForm(UserCreationForm):
 	
 	class Meta:
 		model = User
-		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+		fields = ('username', 'email', 'password1', 'password2')
