e4cf1195cf36bb7e3e03f4aeccfbfb17f5361f37
alexHooper <alexmohack@gmail.com>
2018-09-28 14:40:27 +0530

    start profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,4 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
