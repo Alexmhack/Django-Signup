02c418f923bda18c3a1975694e2c07c17a08c38e
alexHooper <alexmohack@gmail.com>
2018-09-28 19:07:53 +0530

    correct the .get error

--- profiles/utils.py
+++ profiles/utils.py
@@ -2,9 +2,10 @@ import requests
 
 def get_location_from_ip(ip_address):
 	try:
-		response = requests.get(f"http://ip-api.com/json/{ip_address}")
+		response = requests.get(f"http://ip-api.com/json/{ip_address}").json()
 		city = response.get("city")
 		country_code = response.get("countryCode")
 		return f"{city} - {country_code}"
 	except Exception as e:
+		print(e)
 		return "Failed fetching location"
