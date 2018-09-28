94f7bf424b6db891b90cd03100d8fe3e64999b3a
alexHooper <alexmohack@gmail.com>
2018-09-28 15:47:27 +0530

    add attrs to form fields

--- profiles/forms.py
+++ profiles/forms.py
@@ -3,6 +3,52 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
+	username = forms.CharField(
+		label='',
+		max_length=30,
+		min_length=5,
+		required=True,
+		widget=forms.TextInput(
+			attrs={
+				"placeholder": "Username",
+				"class": "form-control"
+			}
+		)
+	)
+
+	password1 = forms.CharField(
+		label='',
+		max_length=30,
+		min_length=8,
+		required=True,
+		widget=forms.PasswordInput(
+			attrs={
+				"placeholder": "Password",
+				"class": "form-control"
+			}
+		)
+	)
+
+	password2 = forms.CharField(
+		label='',
+		max_length=30,
+		min_length=8,
+		required=True,
+		widget=forms.PasswordInput(
+			attrs={
+				"placeholder": "Confirm Password",
+				"class": "form-control"
+			}
+		)
+	)
+
+	location = forms.CharField(
+		label='',
+		max_length=30,
+		required=False,
+		widget=forms.HiddenInput()
+	)
+
 	class Meta:
 		model = User
-		fields = ('username', 'password1', 'password2')
+		fields = ('username', 'password1', 'password2', 'location')
