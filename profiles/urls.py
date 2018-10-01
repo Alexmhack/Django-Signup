61ce82219040b5f4509c1665b4ff2858f3d99d84
alexHooper <alexmohack@gmail.com>
2018-10-01 17:06:54 +0530

    add urls for user activation

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import signup, ProfileUpdateView
+from .views import (
+	signup, ProfileUpdateView, account_activation_sent_view, activate
+)
 
 urlpatterns = [
 	path('', signup, name='profile-signup'),
 	path('<int:pk>/edit/', ProfileUpdateView.as_view(), name='profile-edit'),
+]
+
+urlpatterns += [
+	path('account-activation-sent/', account_activation_sent_view, name='account-activation-sent'),
+	re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+		activate, name='activate'),
 ]
